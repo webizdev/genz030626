f2126cf8a88b49430c518744cdde9be4746fbaf1
webiz_dev <arifinamruna2@gmail.com>
2026-06-03 09:24:44 +0800

    Update database name to genz_db

--- api.php
+++ api.php
@@ -16,8 +16,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 // Database Configuration
 $db_host = 'website-database-iqd1kg';
-$db_name = 'web_db';
-$db_user = 'web_db';
+$db_name = 'genz_db';
+$db_user = 'genz_db';
 $db_pass = '@Qweasz123';
 
 // Connect to MySQL
